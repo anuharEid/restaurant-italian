92b774bd34a328192a2684434ca73b51617ec3f2
Anuhar Eid <anuhar_eid07@hotmail.com>
2022-12-18 13:17:44 +0900

    agregue hasta el video 1:45:57

--- js/app.js
+++ js/app.js
@@ -17,15 +17,24 @@ const botonCerrar = () =>{
      const overlay = document.createElement('div');
      overlay.classList.add('pantalla-completa');
      const body = document.querySelector('body');
+     if(document.querySelectorAll('.pantalla-completa').length > 0) return;
      body.appendChild(overlay);
      btnCerrar.textContent = 'x';
      btnCerrar.classList.add('btn-cerrar');
      navegacion.appendChild(btnCerrar);
-     cerrarMenu(btnCerrar);
+     cerrarMenu(btnCerrar,overlay);
 }
 
-const cerrarMenu = (boton) =>{
+const cerrarMenu = (boton, overlay) =>{
     boton.addEventListener('click',()=>{
         navegacion.classList.add('ocultar');
+        overlay.remove();
+        boton.remove();
     });
+
+    overlay.onclick = function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar')
+        boton.remove();
+    }
 }
